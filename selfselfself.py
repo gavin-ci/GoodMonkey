6f17c6db186a309632ae9e46f7060e0ba49da46b
gavin-ci <89778659+gavin-ci@users.noreply.github.com>
2022-05-03 18:15:25 +0200

    Add files via upload

--- selfselfself.py
+++ selfselfself.py
@@ -16,16 +16,17 @@ class baseFile():
       
         pass  
 
-#EXAMPLE
+#EXMAPLE
 Sol = baseFile("Sol Badguy", "TitleDiff","Pyro", "Claymore", "Junkyard Dog", 500, 25, 30, "Pyro DMG", "Gunflame", 55, "Heavy Mob Cemetary", 110)
 
 #character list starts here:
 Diluc = baseFile("Diluc Ragnvindr", "The Dark Side Of Dawn","Pyro", "Claymore", "Wolf's Gravestone", 1011, 26, 61, "Crit Rate", "Searing Onslaught", 25, "Dawn", 53)
 YaeMiko = baseFile("Yae Miko", "Astute Amusement", "Electro", "Catalyst", "Kagura's Verity", 807, 26, 44, "Crit Rate", "Yakan Evocation: Sesshou Sakura", 24, "Great Secret Art: Tenko Kenshin", 67)
-
+Ayaka = baseFile("kamisato Ayaka", "Frostflake Heron","Cryo", "Sword", "Mistsplitter Reforged", 1001, 27, 61, "Crit DMG", "Kamisato Art: Hyouka", 64, "Kamisato Art: Soumetsu", 75)
 
 
 #printing character lists starts here:
 #print("name = ", Sol.name, "\ntitle = ", Sol.title, "\nvision = ",Sol.vision,"\nweapon type = ",Sol.weaponType,"\nsignature weapon = ",Sol.signatureWeapon, "\nbaseHp = ", Sol.baseHp, "\nbaseAtk = ", Sol.baseAtk, "\nbaseDef = ", Sol.baseDef, "\nspecial stat = ", Sol.special, "\nskill = ", Sol.skill, "\nskill DMG = ", Sol.skillDmg, "\nburst = ", Sol.burst, "\nburst DMG = ", Sol.burstDmg, "\n")
 print("name = ",Diluc.name,"\ntitle = ", Diluc.title, "\nvision = ",Diluc.vision,"\nweapon type = ",Diluc.weaponType,"\nsignature weapon = ",Diluc.signatureWeapon,"\nbaseHp = ",Diluc.baseHp,"\nbaseAtk = ",Diluc.baseAtk,"\nbaseDef = ",Diluc.baseDef,"\nspecial stat = ",Diluc.special,"\nskill = ",Diluc.skill,"\nskill DMG = ",Diluc.skillDmg,"\nburst = ",Diluc.burst,"\nburst DMG = ",Diluc.burstDmg,"\n")
 print("name = ",YaeMiko.name,"\ntitle = ", YaeMiko.title, "\nvision = ",YaeMiko.vision,"\nweapon type = ",YaeMiko.weaponType,"\nsignature weapon = ",YaeMiko.signatureWeapon,"\nbaseHp = ",YaeMiko.baseHp,"\nbaseAtk = ",YaeMiko.baseAtk,"\nbaseDef = ",YaeMiko.baseDef,"\nspecial stat = ",YaeMiko.special,"\nskill = ",YaeMiko.skill,"\nskill DMG = ",YaeMiko.skillDmg,"\nburst = ",YaeMiko.burst,"\nburst DMG = ",YaeMiko.burstDmg,"\n")
+print("name = ", Ayaka.name, "\ntitle = ", Ayaka.title, "\nvision = ",Ayaka.vision,"\nweapon type = ",Ayaka.weaponType,"\nsignature weapon = ",Ayaka.signatureWeapon, "\nbaseHp = ", Ayaka.baseHp, "\nbaseAtk = ", Ayaka.baseAtk, "\nbaseDef = ", Ayaka.baseDef, "\nspecial stat = ", Ayaka.special, "\nskill = ", Ayaka.skill, "\nskill DMG = ", Ayaka.skillDmg, "\nburst = ", Ayaka.burst, "\nburst DMG = ", Ayaka.burstDmg, "\n")
